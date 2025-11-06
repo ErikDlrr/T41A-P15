fdd30a4c0b366c24e9099592487ea7ccaaefcd4a
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2025-11-05 19:14:59 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO productos (nombre, precio, stock) VALUES
+('Laptop', 8500.00, 5),     
+('Monitor', 1999.99, 12),
+('Mouse', 150.00, 30),
+('Teclado', 299.99, 3);     
+
+INSERT INTO departamentos (id, nombre) VALUES
+(1, 'Ventas'),
+(2, 'TI'),
+(3, 'RRHH');
+
+INSERT INTO empleados (nombre, departamento_id) VALUES
+('Erik', 1),
+('Nestor', 2),
+('Ricardo', 2),
+('Claudia', 1),
+('Andres', 2);
